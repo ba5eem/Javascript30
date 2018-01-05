da44e885f6cf0b9881883c24fb407c69826a4c72
Ba5eem <baseem@gmail.com>
2018-01-04 16:19:44 -1000

    some touch

---
+++ drumkit/drumkit.js
@@ -0,0 +1,35 @@
+
+window.addEventListener('keydown', function(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`audio-board[data-key="${e.keyCode}"]`);
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+})
+
+window.addEventListener('click', function(e){
+  const audio = document.querySelector(`audio[id="${e.target.id}"]`);
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+})
+
+
+const toggle = document.getElementsByClassName("toggle");
+
+for(var i = 0; i < toggle.length; i++){
+  toggle[i].addEventListener('click', function(){
+  console.log(this.checked)
+  })
+}
+
+const kitbuttons = document.getElementsByClassName("audio-kit-buttons");
+
+for(var i = 0; i < kitbuttons.length; i++){
+  kitbuttons[i].addEventListener('click', function(){
+  console.log(this.id)
+  })
+}
+
+
+
